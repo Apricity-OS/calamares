2cdd50648a4e1b20079b5a08c8eab35b18d46454
Alex Gajewski <agajews@gmail.com>
2016-04-08 13:32:02 -0500

    Initial Commit

---
+++ src/calamares/build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6428e.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_6428e.dir/feature_tests.cxx.o"
+  "cmTC_6428e.pdb"
+  "cmTC_6428e"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_6428e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/calamares/build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6428e.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_6428e.
+# This may be replaced when dependencies are built.
